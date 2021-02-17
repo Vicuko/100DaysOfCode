8ea2a93c850e66ab2354d70bf8f6f7fc08dfbc4d
Vicuko <victorc.fernandez@gmail.com>
2021-02-17 16:02:47 +0100

    Complete exercise 1

---
+++ Day13/Day13-1-Exercise/main.py
@@ -0,0 +1,8 @@
+number = int(input("Which number do you want to check?"))
+
+# The code was trying to assign 0 to the result of the modulo.
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+  

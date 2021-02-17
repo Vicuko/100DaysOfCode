a57f3263966c2f33f1f57aa6bbbc8fd2a52b8733
Vicuko <victorc.fernandez@gmail.com>
2021-02-17 16:05:54 +0100

    Complete exercise 2

---
+++ Day13/Day13-2-Exercise/main.py
@@ -0,0 +1,16 @@
+# Here the input wasn't converted to an integer, which is required
+# to later carry out the modulo operation on the year.
+
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
+  

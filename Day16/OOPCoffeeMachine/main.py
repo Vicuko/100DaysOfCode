18d48f868b368487e14a8f7bcb14245496d93da2
Vicuko <victorc.fernandez@gmail.com>
2021-02-21 13:11:07 +0100

    Complete Coffee Machine with OOP

--- Day16/OOPCoffeeMachine/main.py
+++ Day16/OOPCoffeeMachine/main.py
@@ -2,3 +2,33 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+class CoffeeMachine:
+
+    def __init__(self):
+        self.on = True
+        self.menu = Menu()
+        self.coffee_maker = CoffeeMaker()
+        self.money_machine = MoneyMachine()
+
+    def start(self):
+        while self.on:
+            print("Welcome to your coffee machine!\n")
+            self.coffee_maker.report()
+            self.money_machine.report()
+            request = input(f"\nWhat would you like? ({self.menu.get_items()}):").lower()
+            if request == "off":
+                self.on = False
+                print ("Bye bye!")
+            else:
+                order = self.menu.find_drink(request)
+                if order:
+                    sufficient_resources = self.coffee_maker.is_resource_sufficient(order)
+                    if sufficient_resources:
+                        cost = order.cost
+                        print (f"\nIt will be {self.money_machine.CURRENCY}{cost}")
+                        if self.money_machine.make_payment(cost):
+                            self.coffee_maker.make_coffee(order)
+
+my_coffee_machine = CoffeeMachine()
+my_coffee_machine.start()
+

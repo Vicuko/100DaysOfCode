ad3f76208036e621e44cc379c7f21fc04512939e
Vicuko <victorc.fernandez@gmail.com>
2021-02-21 15:55:29 +0100

    Modify the way questions are stored in the question bank

--- Day17/QuizGame/main.py
+++ Day17/QuizGame/main.py
@@ -4,7 +4,12 @@ from data import question_data
 question_bank = list()
 
 for line in question_data:
-    question_bank.append(Question(line["text"], line["answer"]))
+    question_text = line["text"]
+    question_answer = line["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+    # Another way of writing it down:
+    # question_bank.append(Question(line["text"], line["answer"]))
 
 # print (question_bank)
 #

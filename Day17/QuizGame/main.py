a5a32a7c14242937f3bbf37bba9d2135d0b6c2e2
Vicuko <victorc.fernandez@gmail.com>
2021-02-21 16:28:04 +0100

    Create method for retrieving next question

--- Day17/QuizGame/main.py
+++ Day17/QuizGame/main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = list()
 
@@ -8,6 +9,16 @@ for line in question_data:
     question_answer = line["answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+def next_question():
+    next_question = quiz.question_list[quiz.question_number].text
+    quiz.question_number += 1
+    return next_question
+
+next_answer = input(f"{next_question()} True or False? ")
+
     # Another way of writing it down:
     # question_bank.append(Question(line["text"], line["answer"]))
 

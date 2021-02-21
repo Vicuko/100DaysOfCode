de9467a194211c60436468b8f6f354125c2da231
Vicuko <victorc.fernandez@gmail.com>
2021-02-21 10:44:51 +0100

    Modify 1st column alignment

--- Day16/Day16Start/main.py
+++ Day16/Day16Start/main.py
@@ -15,4 +15,5 @@ from prettytable import PrettyTable
 table = PrettyTable()
 table.add_column("Pokemon Name", ["Pikachu","Squirtle","Charmander","Bulbasaur"])
 table.add_column("Type", ["Electric","Water","Fire","Grass"])
+table.align["Pokemon Name"]="r"
 print(table)

f334eb81b55eef8c4fad849de90c6bb5f06fa5ab
Vicuko <victorc.fernandez@gmail.com>
2021-02-21 10:42:17 +0100

    Add columns to show Pokemon data with corresponding type

--- Day16/Day16Start/main.py
+++ Day16/Day16Start/main.py
@@ -13,3 +13,6 @@
 from prettytable import PrettyTable
 
 table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu","Squirtle","Charmander","Bulbasaur"])
+table.add_column("Type", ["Electric","Water","Fire","Grass"])
+print(table)

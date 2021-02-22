2c5dcb021edd8f2dc719bb3e8c4aef5f4607bd3a
Vicuko <victorc.fernandez@gmail.com>
2021-02-22 14:15:14 +0100

    Import heroes module and print a hero name

--- Day18/Day18Start/main.py
+++ Day18/Day18Start/main.py
@@ -1,44 +1,48 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# tim = Turtle()
+# tom = Turtle()
+#
+# tim.shape("turtle")
+# tim.color("blue")
+# tom.shape("circle")
+# tom.color("green")
+#
+# for _ in range(9):
+#     tim.left(30)
+#     tim.forward(50)
+#     tim.left(150)
+#     tim.forward(50)
+#     tim.right(100)
+#
+# tim.color("red")
+# for _ in range(9):
+#     tim.left(30)
+#     tim.forward(70)
+#     tim.left(150)
+#     tim.forward(70)
+#     tim.right(100)
+#
+# for _ in range(4):
+#     tom.right(90)
+#     tom.forward(100)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# screen = Screen()
+# screen.exitonclick()
 
-tim = Turtle()
-tom = Turtle()
 
-tim.shape("turtle")
-tim.color("blue")
-tom.shape("circle")
-tom.color("green")
-
-for _ in range(9):
-    tim.left(30)
-    tim.forward(50)
-    tim.left(150)
-    tim.forward(50)
-    tim.right(100)
-
-tim.color("red")
-for _ in range(9):
-    tim.left(30)
-    tim.forward(70)
-    tim.left(150)
-    tim.forward(70)
-    tim.right(100)
-
-for _ in range(4):
-    tom.right(90)
-    tom.forward(100)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-screen = Screen()
-screen.exitonclick()
+import heroes
+print(heroes.gen())

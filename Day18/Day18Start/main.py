c67d5e486cf2746d523bdb0337cb8dd3901d3db2
Vicuko <victorc.fernandez@gmail.com>
2021-02-22 13:41:40 +0100

    Add tom the turtle to draw a square

--- Day18/Day18Start/main.py
+++ Day18/Day18Start/main.py
@@ -1,9 +1,14 @@
 from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
+tom_the_turtle = Turtle()
+
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("blue")
-for _ in range(10):
+tom_the_turtle.shape("circle")
+tom_the_turtle.color("green")
+
+for _ in range(9):
     timmy_the_turtle.left(30)
     timmy_the_turtle.forward(50)
     timmy_the_turtle.left(150)
@@ -11,14 +16,16 @@ for _ in range(10):
     timmy_the_turtle.right(100)
 
 timmy_the_turtle.color("red")
-for _ in range(10):
+for _ in range(9):
     timmy_the_turtle.left(30)
     timmy_the_turtle.forward(70)
     timmy_the_turtle.left(150)
     timmy_the_turtle.forward(70)
     timmy_the_turtle.right(100)
 
-
+for _ in range(4):
+    tom_the_turtle.right(90)
+    tom_the_turtle.forward(100)
 
 
 

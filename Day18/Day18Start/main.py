4723e8b37b713fd3e1727b66bf7ea750f95d799e
Vicuko <victorc.fernandez@gmail.com>
2021-02-22 14:26:54 +0100

    Create function for drawing discontinuous lines

--- Day18/Day18Start/main.py
+++ Day18/Day18Start/main.py
@@ -1,48 +1,55 @@
-# from turtle import Turtle, Screen
-#
-# tim = Turtle()
-# tom = Turtle()
-#
-# tim.shape("turtle")
-# tim.color("blue")
-# tom.shape("circle")
-# tom.color("green")
-#
-# for _ in range(9):
-#     tim.left(30)
-#     tim.forward(50)
-#     tim.left(150)
-#     tim.forward(50)
-#     tim.right(100)
-#
-# tim.color("red")
-# for _ in range(9):
-#     tim.left(30)
-#     tim.forward(70)
-#     tim.left(150)
-#     tim.forward(70)
-#     tim.right(100)
-#
-# for _ in range(4):
-#     tom.right(90)
-#     tom.forward(100)
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# screen = Screen()
-# screen.exitonclick()
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tom = Turtle()
+
+tim.shape("turtle")
+tim.color("blue")
+tom.shape("circle")
+tom.color("green")
+
+def disc_line(turtle, length):
+    remainder = length % 10
+    lines_num = length // 10
+    for i in range(lines_num):
+        if i % 2 == 0:
+            turtle.up()
+        else:
+            turtle.down()
+        turtle.forward(10)
+    if turtle.isdown():
+        turtle.up()
+    else:
+        turtle.down()
+    turtle.forward(remainder)
+
+for _ in range(9):
+    tim.left(30)
+    disc_line(tim, 50)
+    tim.left(150)
+    disc_line(tim, 50)
+    tim.right(100)
+
+tim.color("red")
+for _ in range(9):
+    tim.left(30)
+    tim.forward(70)
+    tim.left(150)
+    tim.forward(70)
+    tim.right(100)
+
+for _ in range(4):
+    tom.right(90)
+    tom.forward(100)
 
 
-import heroes
-print(heroes.gen())
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()

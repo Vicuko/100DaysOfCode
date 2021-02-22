8aec54f6e9c8bca768d910391f6a06d475866ac1
Vicuko <victorc.fernandez@gmail.com>
2021-02-22 14:07:56 +0100

    Simplify turtles' name

--- Day18/Day18Start/main.py
+++ Day18/Day18Start/main.py
@@ -1,31 +1,31 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-tom_the_turtle = Turtle()
+tim = Turtle()
+tom = Turtle()
 
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("blue")
-tom_the_turtle.shape("circle")
-tom_the_turtle.color("green")
+tim.shape("turtle")
+tim.color("blue")
+tom.shape("circle")
+tom.color("green")
 
 for _ in range(9):
-    timmy_the_turtle.left(30)
-    timmy_the_turtle.forward(50)
-    timmy_the_turtle.left(150)
-    timmy_the_turtle.forward(50)
-    timmy_the_turtle.right(100)
+    tim.left(30)
+    tim.forward(50)
+    tim.left(150)
+    tim.forward(50)
+    tim.right(100)
 
-timmy_the_turtle.color("red")
+tim.color("red")
 for _ in range(9):
-    timmy_the_turtle.left(30)
-    timmy_the_turtle.forward(70)
-    timmy_the_turtle.left(150)
-    timmy_the_turtle.forward(70)
-    timmy_the_turtle.right(100)
+    tim.left(30)
+    tim.forward(70)
+    tim.left(150)
+    tim.forward(70)
+    tim.right(100)
 
 for _ in range(4):
-    tom_the_turtle.right(90)
-    tom_the_turtle.forward(100)
+    tom.right(90)
+    tom.forward(100)
 
 
 

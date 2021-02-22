9193f6da931e8447ff8e52f6171ea5cebdcc9ccc
Vicuko <victorc.fernandez@gmail.com>
2021-02-22 14:55:42 +0100

    Generate function to create a random walk

--- Day18/Day18Start/main.py
+++ Day18/Day18Start/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random
 
 tim = Turtle()
 tom = Turtle()
@@ -7,6 +8,14 @@ tim.shape("turtle")
 tim.color("blue")
 tom.shape("circle")
 tom.color("green")
+
+colours_palette = ["blue","chartreuse", "magenta", "sky blue", "yellow", "black", "dodger blue"]
+
+def draw_shape(turtle, side_num):
+    angle = 360 / side_num
+    for _ in range(side_num):
+        turtle.right(angle)
+        turtle.forward(100)
 
 def disc_line(turtle, length):
     remainder = length % 10
@@ -22,6 +31,13 @@ def disc_line(turtle, length):
     else:
         turtle.down()
     turtle.forward(remainder)
+
+def random_walk(turtle, length, colors = ["blue"]):
+    angles = list(range(0,360,90))
+    turtle.color(random.choice(colors))
+    turtle.right(random.choice(angles))
+    turtle.forward(length)
+
 
 # for _ in range(9):
 #     tim.left(30)
@@ -45,22 +61,18 @@ def disc_line(turtle, length):
 #     tom.right(90)
 #     tom.forward(100)
 
-#Draw several shapes overlaid:
+# Draw several shapes overlaid:
+# max_sides_num = 12
+#
+# for side_num in range(3, max_sides_num+1):
+#     tim.color(colours_palette[side_num % len(colours_palette)])
+#     draw_shape(tim, side_num)
 
-max_sides_num = 12
-
-colours_palette = ["blue","chartreuse", "magenta", "sky blue", "yellow", "black", "dodger blue"]
-
-def draw_shape(turtle, side_num):
-    angle = 360 / side_num
-    for _ in range(side_num):
-        turtle.right(angle)
-        turtle.forward(100)
-
-for side_num in range(3, max_sides_num+1):
-    tim.color(colours_palette[side_num % len(colours_palette)])
-    draw_shape(tim, side_num)
-
+# Draw a random walk:
+tim.speed("fast")
+tim.width(10)
+for _ in range(100):
+    random_walk(tim, 20, colours_palette)
 
 
 

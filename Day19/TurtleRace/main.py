59ecdfbd9a7be6140c2ae35114b9432858e42a3d
Vicuko <victorc.fernandez@gmail.com>
2021-02-23 23:14:21 +0100

    Complete turtle race code

--- Day19/TurtleRace/main.py
+++ Day19/TurtleRace/main.py
@@ -1,14 +1,19 @@
 import turtle as t
+import random
 
 screen = t.Screen()
 screen_dimensions = (500, 400)
 screen.setup(width = screen_dimensions[0], height = screen_dimensions[1])
+
+race_on = False
+winner = None
 user_bet = screen.textinput(title = "Make your bet!", prompt="Which turtle will win the race? Enter a color: ")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
 turtles = []
 turtles_distance = 30
 lowest_y = -((len(colors)-1) * turtles_distance)/2
+goal_x = screen_dimensions[0]/2 - 30
 for i in range(len(colors)):
     new_turtle = t.Turtle(shape="turtle")
     new_turtle.up()
@@ -16,8 +21,23 @@ for i in range(len(colors)):
     turtles.append(new_turtle)
     new_turtle.goto(x=-screen_dimensions[0]/2 + 20, y = lowest_y + turtles_distance * i)
 
-# tim = t.Turtle(shape="turtle")
-# tim.up()
-# tim.goto(x=-screen_dimensions[0]/2 + 20, y = 0)
+if user_bet:
+    race_on = True
+
+while race_on:
+    for turtle in turtles:
+        move = random.randint(0 , 10)
+        turtle.forward(move)
+        if turtle.xcor() >= goal_x:
+            winner = turtle.pencolor()
+            race_on = False
+            break
+
+print (f"The winner is: {winner}")
+if winner == user_bet:
+    print ("Woohoo!! You won!!")
+else:
+    print ("You lost... Better luck next time!")
+
 
 screen.exitonclick()

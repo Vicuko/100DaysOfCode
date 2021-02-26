c68fd09098667a4ca473c9dbac85fec5e552aeb5
Vicuko <victorc.fernandez@gmail.com>
2021-02-26 23:28:57 +0100

    Store font conditions as constants in the scoreboard file

--- Day20/SnakeGame/scoreboard.py
+++ Day20/SnakeGame/scoreboard.py
@@ -1,13 +1,17 @@
 from turtle import Turtle
 
+ALIGNMENT = "center"
+FONT = "Arial"
+FONT_SIZE = 16
+FONT_TYPE = "normal"
+
 class Scoreboard(Turtle):
-    def __init__(self, color = "white", size = 16):
+    def __init__(self, color = "white"):
         super().__init__()
         self.score = 0
         self.hideturtle()
         self.color(color)
         self.setposition(0, 270)
-        self.font_size = size
         self.print_score()
 
     def increase_score(self):
@@ -21,4 +25,4 @@ class Scoreboard(Turtle):
     def print_score(self):
         score_text = f"Score: {self.score}"
         self.clear()
-        self.write(score_text, align="center", font=("Arial", self.font_size, "normal"))
+        self.write(score_text, align="center", font=(FONT, FONT_SIZE, FONT_TYPE))

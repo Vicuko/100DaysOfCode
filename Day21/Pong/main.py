15df7ade3c89d0d18eb7682cab22614daaab98f5
Vicuko <victorc.fernandez@gmail.com>
2021-02-28 00:28:36 +0100

    Adjust paddle boundaries to screen size

--- Day21/Pong/main.py
+++ Day21/Pong/main.py
@@ -1,18 +1,29 @@
 from turtle import Screen
 from paddle import Paddle
+import time
 
 GAME_NAME = "TURBO PONG"
 BACKGROUND_COLOR = "BLACK"
 #TODO 1. Create Screen - Done
 screen = Screen()
 screen.title(GAME_NAME)
-screen.setup(height=1080, width=1920)
+screen.setup(height=1.0, width=1.0)
 screen.bgcolor(BACKGROUND_COLOR)
+screen.tracer(n=0)
 screen.listen()
 
 #TODO 2. Create and move a paddle
-player1 = Paddle()
-player2 = Paddle()
+#Prompt user for player name
+screen_height = screen.window_height()
+print ("Screen height: ", screen_height)
+player1 = Paddle(screen_height = screen_height)
+player2 = Paddle(screen_height = screen_height)
+
+screen.onkey(fun=player1.move_up, key="w")
+screen.onkey(fun=player1.move_down, key="s")
+screen.onkey(fun=player2.move_up, key="Up")
+screen.onkey(fun=player2.move_down, key="Down")
+
 #TODO 3. Create another paddle
 #TODO 4. Create the ball and make it move
 #TODO 5. Detect collision with the wall and bounce
@@ -20,6 +31,11 @@ player2 = Paddle()
 #TODO 7. Detect when paddle misses
 #TODO 8. Keep Score
 
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+    #ball.move()
 
 
 
--- Day21/Pong/paddle.py
+++ Day21/Pong/paddle.py
@@ -2,29 +2,34 @@ from turtle import Turtle
 
 class Paddle:
 
-    def __init__(self, color = "white", length=4):
+    def __init__(self, screen_height, color = "white", length=4):
         self.body = []
+        self.screen_height = screen_height
         self.create_paddle(length, color)
 
     def create_paddle(self, length, color):
         head_pos = (length * 20) // 2 - 10
+        print(f"Head pos: {head_pos}")
         for index in range (length):
             new_part = Turtle(shape="square")
             new_part.color(color)
             new_part.penup()
             new_part.setpos(x = 0, y = head_pos - 20 * index)
+            self.body.append(new_part)
 
     def move_up(self):
-        highest_part_y = self.body[-1].ycor()
-        if highest_part_y + 20 < 540:
-        for part in self.body:
-            part_x = part.xcor()
-            part_y = part.ycor()
-            part.goto(part_x, part_y + 20)
+        highest_part_y = self.body[0].ycor()
+        if highest_part_y + 20 < self.screen_height/2:
+            print(highest_part_y - 20)
+            for part in self.body:
+                part_x = part.xcor()
+                part_y = part.ycor()
+                part.goto(part_x, part_y + 20)
 
     def move_down(self):
         lowest_part_y = self.body[-1].ycor()
-        if lowest_part_y - 20 >= -540:
+        if lowest_part_y - 20 >= -(self.screen_height/2):
+            print(lowest_part_y - 20)
             for part in self.body:
                 part_x = part.xcor()
                 part_y = part.ycor()

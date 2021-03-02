adf142c2059be0d8a8edef1d0f0af895b2346b23
Vicuko <victorc.fernandez@gmail.com>
2021-03-02 23:46:36 +0100

    Create basic file for carrying out operations with files in python

---
+++ Day24/Day24Start/main.py
@@ -0,0 +1,16 @@
+# To read a file we can use the built in open function:
+with open("my_file.txt", mode="r") as file:
+    contents = file.read()
+    lines = contents.split("\n")
+    counter = 1
+    for line in lines:
+        print(f"Line{counter}:{line}")
+        counter += 1
+
+# To write into a file: (remember, it deletes everything in the file)
+with open("my_file.txt", mode="w") as file:
+    file.write("New Text!!!")
+
+# To append into a file: (remember, it doesnt' include line breaks)
+with open("my_file.txt", mode="a") as file:
+    file.write("\nNew Text 2!!")

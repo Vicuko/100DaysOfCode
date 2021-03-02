4fefb7d648d244e9623f03974f40cc835539c95e
Vicuko <victorc.fernandez@gmail.com>
2021-03-03 00:14:51 +0100

    Simplify file renaming

--- Day24/Day24Start/main.py
+++ Day24/Day24Start/main.py
@@ -1,5 +1,6 @@
+FILE_LOCATION = "my_file.txt"
 # To read a file we can use the built in open function:
-with open("my_file.txt", mode="r") as file:
+with open(FILE_LOCATION, mode="r") as file:
     contents = file.read()
     lines = contents.split("\n")
     counter = 1
@@ -8,9 +9,9 @@ with open("my_file.txt", mode="r") as file:
         counter += 1
 
 # To write into a file: (remember, it deletes everything in the file)
-with open("my_file.txt", mode="w") as file:
+with open(FILE_LOCATION, mode="w") as file:
     file.write("New Text!!!")
 
 # To append into a file: (remember, it doesnt' include line breaks)
-with open("my_file.txt", mode="a") as file:
+with open(FILE_LOCATION, mode="a") as file:
     file.write("\nNew Text 2!!")

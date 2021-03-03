93351b0371d04ee67e0bbe9fc5a836c6413a9f0c
Vicuko <victorc.fernandez@gmail.com>
2021-03-03 01:17:24 +0100

    Correct output path for letters

--- Day24/MailMerge/main.py
+++ Day24/MailMerge/main.py
@@ -8,20 +8,19 @@
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 NAMES_DIR = "./Input/Names/invited_names.txt"
 LETTER_DIR = "./Input/Letters/starting_letter.txt"
+
 names = None
 letter = None
 
 with open(NAMES_DIR, mode="r") as file:
-    lines = file.readlines()
-    names = []
-    for line in lines:
-        names.append(line.strip())
+    names = file.readlines()
 
 with open(LETTER_DIR, mode="r") as file:
     letter = file.read()
 
 for name in names:
-    new_file_name = f"letter_for_{name}.txt"
-    letter_text = letter.replace("[name]", f"{name}")
-    with open(new_file_name, mode="w") as file:
+    stripped_name = name.strip()
+    new_file_dir = f"./Output/ReadyToSend/letter_for_{stripped_name}.txt"
+    letter_text = letter.replace("[name]", f"{stripped_name}")
+    with open(new_file_dir, mode="w") as file:
         file.write(letter_text)

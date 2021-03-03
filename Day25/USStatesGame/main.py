804e669466e2fff4663135dcacf65c90454aaec6
Vicuko <victorc.fernandez@gmail.com>
2021-03-03 19:54:16 +0100

    Complete States game

--- Day25/USStatesGame/main.py
+++ Day25/USStatesGame/main.py
@@ -1,16 +1,40 @@
 import turtle
+import pandas as pd
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
+screen.setup(width=740, height=500)
 image = "./blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
-
 
 # def get_mouse_coords(x,y):
 #     print (x,y)
 #
 # turtle.onscreenclick(get_mouse_coords)
 
+states_writer = turtle.Turtle()
+states_writer.penup()
+states_writer.hideturtle()
+correct_states = []
+states_data = pd.read_csv("./50_states.csv")
+
+while len(correct_states) < 50:
+
+    if correct_states:
+        answer_state = screen.textinput(title=f"{len(correct_states)}/50 States Correct", prompt="What's another state's name?").title()
+    else:
+        answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?").title()
+
+    answer_in_tables = (states_data.state == answer_state)
+    if answer_in_tables.any() and answer_state not in correct_states:
+        x_coord = int(states_data[answer_in_tables].x)
+        y_coord = int(states_data[answer_in_tables].y)
+        states_writer.setposition(x= x_coord, y=y_coord)
+        states_writer.write(answer_state, align="center")
+        correct_states.append(answer_state)
+
+    # print (answer_state)
+#
+
 turtle.mainloop()
-# screen.exitonclick()

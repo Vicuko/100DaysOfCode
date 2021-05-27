71dd87f6834123db4576917a810cc39b45d7ad68
Vicuko <victorc.fernandez@gmail.com>
2021-05-28 00:03:00 +0200

    Improve exercise 4

--- Day26/Day26-4-Exercise/main.py
+++ Day26/Day26-4-Exercise/main.py
@@ -3,8 +3,7 @@ sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
 
 # Write your code below:
 
-words = sentence.split(" ")
-result = {word:len(word) for word in words}
+result = {word:len(word) for word in sentence.split()}
 
 print(result)
 

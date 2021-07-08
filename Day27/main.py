f8905f5f3ae1f19e5a6ad968591d085709bcdc5d
Vicuko <victorc.fernandez@gmail.com>
2021-07-09 00:40:17 +0200

    Create basic function using *args

--- Day27/main.py
+++ Day27/main.py
@@ -4,13 +4,9 @@ window = tkinter.Tk()
 window.title("My first Tkinter window")
 window.minsize(width=500, height=300)
 
+#Label
 
-
-
-
-
-
-
-
+my_label = tkinter.Label(text="My super label text", font=("Arial", 24, "italic"))
+my_label.pack(expand=True)
 
 window.mainloop()
---
+++ Day27/playground.py
@@ -0,0 +1,6 @@
+def add(*numbers):
+    added_nums = sum(numbers)
+    return added_nums
+
+result = add(1,2,3,4,5,6)
+print (result)

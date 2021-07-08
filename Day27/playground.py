e7164d436f6e1111b237142dcabb28ac4efb2193
Vicuko <victorc.fernandez@gmail.com>
2021-07-09 00:49:31 +0200

    Add function for **kwargs example

--- Day27/playground.py
+++ Day27/playground.py
@@ -1,6 +1,19 @@
-def add(*numbers):
-    added_nums = sum(numbers)
+def add(*args):
+    # args is a tuple:
+    print(type(args))
+    added_nums = sum(args)
     return added_nums
 
 result = add(1,2,3,4,5,6)
 print (result)
+
+def calculate(**kwargs):
+    # kwargs is a dictionary:
+    print(type(kwargs))
+    print(kwargs)
+
+    for key, value in kwargs.items():
+        print(f"{key} : {value}")
+
+
+calculate(add=3, multiply=5)

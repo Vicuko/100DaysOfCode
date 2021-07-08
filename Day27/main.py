426c3707ea5c33d716bb79c08862096dbcef86a8
Vicuko <victorc.fernandez@gmail.com>
2021-07-09 01:00:23 +0200

    Add button object to tkinter interface

--- Day27/main.py
+++ Day27/main.py
@@ -1,12 +1,24 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first Tkinter window")
 window.minsize(width=500, height=300)
 
 #Label
 
-my_label = tkinter.Label(text="My super label text", font=("Arial", 24, "italic"))
-my_label.pack(expand=True)
+my_label = Label(text="My super label text", font=("Arial", 24, "italic"))
+my_label.pack()
+
+#Several ways to update the text content:
+my_label["text"]="Bla bla bla2"
+my_label.config(text="New Text")
+
+#Button
+
+button = Button(text="Click Me")
+button.pack()
+
+
+
 
 window.mainloop()

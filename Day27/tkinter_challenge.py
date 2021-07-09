0146849d028e97986cfe070d74f280924bf0d40f
Vicuko <victorc.fernandez@gmail.com>
2021-07-09 17:09:45 +0200

    Add padding to window

--- Day27/tkinter_challenge.py
+++ Day27/tkinter_challenge.py
@@ -3,6 +3,8 @@ from tkinter import *
 window = Tk()
 window.title(string = "Challenge Window")
 window.minsize(width=500, height=300)
+# We can also add padding to any element:
+window.config(padx=40, pady=40)
 
 label = Label(text="Label 1", font=("Arial", 24, "italic"))
 label.grid(row=0, column=0)
@@ -13,7 +15,7 @@ button1.grid(row=1, column=1)
 button2 = Button(text="Button 2")
 button2.grid(row=0, column=2)
 
-entry = Entry(width=30)
+entry = Entry(width=10)
 entry.grid(row=2, column=3)
 
 

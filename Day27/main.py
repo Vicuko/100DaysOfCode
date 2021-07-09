3b715a019aee7cf9664d01b52066d003ef8ca627
Vicuko <victorc.fernandez@gmail.com>
2021-07-09 17:00:41 +0200

    Add different options in comment for positioning elements in the window

--- Day27/main.py
+++ Day27/main.py
@@ -8,13 +8,14 @@ window.minsize(width=500, height=300)
 
 my_label = Label(text="My super label text", font=("Arial", 24, "italic"))
 my_label.pack()
+# my_label.place(x=100, y=100)
+# my_label.grid(row=0, column=0)
 
 #Several ways to update the text content:
 my_label["text"]="Bla bla bla2"
 my_label.config(text="New Text")
 
 #Button
-
 def button_clicked():
     # my_label["text"] = "This button just got clicked!"
     # my_label.config(text="This button just got clicked!")
@@ -25,7 +26,6 @@ button = Button(text="Click Me", command=button_clicked)
 button.pack()
 
 #Input field
-
 input = Entry(width=10)
 input.pack()
 

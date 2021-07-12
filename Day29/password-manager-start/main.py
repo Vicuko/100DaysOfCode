069b2b941fc355fae1ff07624927bf83b0544942
Vicuko <victorc.fernandez@gmail.com>
2021-07-12 17:57:55 +0200

    Create function for generating random password formed by letters, numbers and symbols

--- Day29/password-manager-start/main.py
+++ Day29/password-manager-start/main.py
@@ -1,10 +1,35 @@
 from tkinter import *
 from tkinter import messagebox
+from random import randint, choice, choices, shuffle
 
 FONT_NAME = "Arial"
 FONT_SIZE = 14
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+               'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = randint(8,10)
+    nr_symbols = randint(2,4)
+    nr_numbers = randint(2,4)
+
+    random_letters = choices(letters, k=nr_letters)
+    random_symbols = choices(symbols, k=nr_symbols)
+    random_numbers = choices(numbers, k=nr_numbers)
+
+    password_list = random_letters + random_symbols + random_numbers
+
+    # password_list += ([choice(letters) for _ in range(nr_letters)])
+    # password_list += ([choice(symbols) for _ in range(nr_letters)])
+    # password_list += ([choice(numbers) for _ in range(nr_letters)])
+
+    shuffle(password_list)
+    password_list = "".join(password_list)
+    password_entry.delete(0, "end")
+    password_entry.insert(0, string=password_list)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
@@ -25,7 +50,7 @@ def save_password():
             with open("passwords.txt", "a") as file:
                 file.write(f"{website} | {email} | {password}\n")
                 web_entry.delete(0, "end")
-                email_entry.delete(0, "end")
+                # email_entry.delete(0, "end")
                 password_entry.delete(0, "end")
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -43,15 +68,15 @@ canvas.grid(row=0, column=1)
 web_label = Label(text="Website:", font=(FONT_NAME, FONT_SIZE))
 web_entry = Entry(width=35)
 web_entry.focus()
-# web_entry.insert(END, string="www.amazingwebsite.com")
+# web_entry.insert(0, string="www.amazingwebsite.com")
 
 email_label = Label(text="Email/Username:", font=(FONT_NAME, FONT_SIZE))
 email_entry = Entry(width=35)
-email_entry.insert(0, string="myamazingemail@amazing.com")
+email_entry.insert(0, string="manolo@amazing.com")
 
 password_label = Label(text="Password:", font=(FONT_NAME, FONT_SIZE))
 password_entry = Entry(width=24)
-generate_button = Button(text="Generate")
+generate_button = Button(text="Generate", command=generate_password)
 add_button = Button(text="Add", width=33, command=save_password)
 
 #Position elements on screen:

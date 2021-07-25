ed29d981c9783e964e05d4b90ad24b5a98df25dd
Vicuko <victorc.fernandez@gmail.com>
2021-07-26 00:22:48 +0200

    Complete exercise 1 for day 30

--- Day30/Day30-1-Exercise/main.py
+++ Day30/Day30-1-Exercise/main.py
@@ -1,41 +1,17 @@
-#Exception Handling
+fruits = ["Apple", "Pear", "Orange"]
+
+#TODO: Catch the exception and make sure the code runs without crashing.
+def make_pie(index):
+    fruit = fruits[index]
+    print(fruit + " pie")
 
 try:
-    file = open("a_file.txt")
-    a_dictionary = {"key": "value"}
-    print(a_dictionary["key"])
-except FileNotFoundError:
-    file = open("a_file.txt", "w")
-    file.write("Something")
-except KeyError as error_message:
-    print(f"The key {error_message} does not exist.")
+    make_pie(4)
+
+except IndexError as error_message:
+    print("Fruit pie")
+
 else:
-    content = file.read()
-    print(content)
-finally:
-    raise TypeError("This is an error that I made up.")
-
-#BMI Example
-
-height = float(input("Height: "))
-weight = int(input("Weight: "))
-
-if height > 3:
-    raise ValueError("Human Height should not be over 3 meters.")
-
-bmi = weight / height ** 2
-print(bmi)
-
-
-
-
-
-
-
-
-
-
-
-
+    print(make_pie(4))
 
 

a8aca2c218a614753a8bcd795cf967d9282f7516
Vicuko <victorc.fernandez@gmail.com>
2021-07-26 11:55:32 +0200

    Include exception handling for the user input

--- Day30/NATOProject/main.py
+++ Day30/NATOProject/main.py
@@ -1,9 +1,19 @@
 import pandas as pd
 nato_data = pd.read_csv("nato_phonetic_alphabet.csv")
 
-data_dict = {row.letter:row.code for index,row in data.iterrows()}
+data_dict = {row.letter:row.code for index,row in nato_data.iterrows()}
 print(data_dict)
 
-word = input("Please introduce the word for the NATO phonetic code:").upper()
-result = [data_dict[letter] for letter in word]
-print (result)
+def generate_phonetics():
+    word = input("Please introduce the word for the NATO phonetic code:").upper()
+    try:
+        result = [data_dict[letter] for letter in word]
+
+    except KeyError as error_message:
+        print(f"{error_message} is not a letter in the alphabet. Please, introduce only letters")
+        generate_phonetics()
+
+    else:
+        print(result)
+
+generate_phonetics()

e3ec6b1075b49c861bf21cc12a1276dd47ea034c
Vicuko <victorc.fernandez@gmail.com>
2021-07-26 11:33:36 +0200

    Correct fruit pie code to include try except block inside function

--- Day30/Day30-1-Exercise/main.py
+++ Day30/Day30-1-Exercise/main.py
@@ -2,16 +2,17 @@ fruits = ["Apple", "Pear", "Orange"]
 
 #TODO: Catch the exception and make sure the code runs without crashing.
 def make_pie(index):
-    fruit = fruits[index]
-    print(fruit + " pie")
+    try:
+        fruit = fruits[index]
 
-try:
-    make_pie(4)
+    except IndexError:
+        print("Fruit pie")
 
-except IndexError as error_message:
-    print("Fruit pie")
+    else:
+        print(fruit + " pie")
 
-else:
-    print(make_pie(4))
+make_pie(2)
+
+
 
 

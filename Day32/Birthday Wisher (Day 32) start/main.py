8b704dd35f94ef6da16f53847e83459fe0534ac5
Vicuko <victorc.fernandez@gmail.com>
2021-07-30 17:30:58 +0200

    Play around with the datetime library

--- Day32/Birthday Wisher (Day 32) start/main.py
+++ Day32/Birthday Wisher (Day 32) start/main.py
@@ -1,13 +1,25 @@
-from smtplib import SMTP
-import passwords
+# from smtplib import SMTP
+# import passwords
+#
+# my_email = "mymail@gmail.com"
+# password = passwords.GMAIL_PASS
+#
+# with SMTP(host="smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(from_addr=my_email, to_addrs="receivermail@gmail.com",
+#                         msg="Subject: Superemail\n\n"
+#                             "<b>Hello</b>")
+#     connection.close()
 
-my_email = "mymail@gmail.com"
-password = passwords.GMAIL_PASS
+import datetime as dt
 
-with SMTP(host="smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email, to_addrs="receivermail@gmail.com",
-                        msg="Subject: Superemail\n\n"
-                            "<b>Hello</b>")
-    connection.close()
+time_now = dt.datetime.now()
+year = time_now.year
+month = time_now.month
+# Day of the week (1-7):
+weekday = time_now.weekday()
+print(type(time_now))
+
+date_of_birth = dt.datetime(day=1, month=6, year=1987, hour=1)
+print(date_of_birth)

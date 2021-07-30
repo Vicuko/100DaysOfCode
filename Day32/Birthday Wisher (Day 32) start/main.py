95168a51297257ae977c85cdd738b828abda4b50
Vicuko <victorc.fernandez@gmail.com>
2021-07-30 17:52:34 +0200

    Use f string for inserting quote into mail

--- Day32/Birthday Wisher (Day 32) start/main.py
+++ Day32/Birthday Wisher (Day 32) start/main.py
@@ -23,5 +23,4 @@ if weekday == 4:
             connection.starttls()
             connection.login(user=my_email, password=password)
             connection.sendmail(from_addr=my_email, to_addrs="receiver@gmail.com",
-                                msg="Subject: Motivation Mail\n\n" +
-                                    str(random_quote))
+                                msg=f"Subject: Motivation Mail\n\n{random_quote}")

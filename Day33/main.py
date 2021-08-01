544cfd5ee341af4c19e10585f1f38c49e0629eb6
Vicuko <victorc.fernandez@gmail.com>
2021-08-01 16:21:59 +0200

    Add starting code for Kanye quotes app

--- Day33/main.py
+++ Day33/main.py
@@ -1,4 +1,13 @@
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
+print(response.status_code)
+
+response.raise_for_status()
+
+data = response.json()
+iss_data = data["iss_position"]
+longitude = iss_data["longitude"]
+latitude = iss_data["latitude"]
+iss_position = (latitude, longitude)
+print(iss_position)

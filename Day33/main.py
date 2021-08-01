4f5f36e524846929d541a149373aa5a954f4de89
Vicuko <victorc.fernandez@gmail.com>
2021-08-01 16:53:01 +0200

    Add initial project for ISS position following

--- Day33/main.py
+++ Day33/main.py
@@ -1,13 +1,34 @@
 import requests
+from datetime import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response.status_code)
+MY_LAT = 43.349590
+MY_LONG = -3.326350
 
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# print(response.status_code)
+#
+# response.raise_for_status()
+#
+# data = response.json()
+# iss_data = data["iss_position"]
+# longitude = iss_data["longitude"]
+# latitude = iss_data["latitude"]
+# iss_position = (latitude, longitude)
+# print(iss_position)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-
 data = response.json()
-iss_data = data["iss_position"]
-longitude = iss_data["longitude"]
-latitude = iss_data["latitude"]
-iss_position = (latitude, longitude)
-print(iss_position)
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
+print(datetime.now().hour)
+
